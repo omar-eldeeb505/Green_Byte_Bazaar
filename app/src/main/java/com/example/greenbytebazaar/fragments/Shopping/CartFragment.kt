6f021e678f4118fa6df895cc02ec7c8be87a25a2
Omar Eldeeb <oeldeeb505@gmail.com>
2025-05-09 17:22:04 +0300

    Bottom_Navegation_commit

---
+++ app/src/main/java/com/example/greenbytebazaar/fragments/Shopping/CartFragment.kt
@@ -0,0 +1,7 @@
+package com.example.greenbytebazaar.fragments.Shopping
+
+import androidx.fragment.app.Fragment
+import com.example.greenbytebazaar.R
+
+class CartFragment:Fragment(R.layout.fragment_cart) {
+}
---
+++ app/src/main/java/com/example/greenbytebazaar/fragments/Shopping/ProfileFragment.kt
@@ -0,0 +1,7 @@
+package com.example.greenbytebazaar.fragments.Shopping
+
+import androidx.fragment.app.Fragment
+import com.example.greenbytebazaar.R
+
+class ProfileFragment:Fragment(R.layout.fragment_profile) {
+}
